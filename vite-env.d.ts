e5138b6f1a06a9af56a4ff049dd650d093b77014
erfanezk <erfan.ezkat@gmail.com>
2026-05-07 01:23:42 +0330

    chore: add image type declarations and update biome rules

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,3 +4,18 @@ declare module '*.mp3' {
   const src: string;
   export default src;
 }
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
